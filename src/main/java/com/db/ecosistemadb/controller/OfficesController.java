eab24685c661e90b86085aeadf867a601c1cece1
Paulo <dpseutroco@gmail.com>
2022-10-20 18:06:13 -0300

    add controller

---
+++ src/main/java/com/db/ecosistemadb/controller/OfficesController.java
@@ -0,0 +1,9 @@
+package com.db.ecosistemadb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/eco")
+public class OfficesController {
+}
